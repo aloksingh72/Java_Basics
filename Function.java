7245a01fbf4412b7f93415807ba2441547f53c42
Alok Singh <aloksingh36757@gmail.com>
2024-05-15 15:10:13 +0530

    new updates

--- Function.java
+++ Function.java
@@ -15,14 +15,15 @@ public class Function {
     //}
 
     // function declarations
-    public static int calculateProduct(int a,int b){
-        return a*b;
-    }
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println("product of two number is :- "+ calculateProduct(a, b));
-    }
+    // public static int calculateProduct(int a,int b){
+    //     return a*b;
+    // }
+    // public static void main(String args[]){
+    //     Scanner sc = new Scanner(System.in);
+    //     int a = sc.nextInt();
+    //     int b = sc.nextInt();
+    //     System.out.println("product of two number is :- "+ calculateProduct(a, b));
     
+    
+
 }
